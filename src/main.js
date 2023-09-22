b1aad80ba328b806f1cddead3b2627f7418db545
Maicon Filippsen <filippsen@universe.ai>
2023-09-22 08:44:35 -0300

    Add initial script entrypoint and app component

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Globals from "../build/WebChat-browser.js";
+
+import { minidenticon } from 'minidenticons'
+
+// Need to call minidenticon to activate it.
+minidenticon();
+
+import * as riot from "riot";
+
+import App from "./app.riot"
+
+const mountApp = riot.component(App);
+const app = mountApp(document.querySelector("#app"), { Globals });
+
+
+import "./main.css";
