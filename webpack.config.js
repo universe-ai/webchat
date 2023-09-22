265682aae6a5dad4b3fd1cfda512dcf3dc97c024
Maicon Filippsen <filippsen@universe.ai>
2023-09-22 08:51:45 -0300

    Add webpack development and release configuration files

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    node: {
+        global: false
+    },
+    mode: "development",
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "main.css"
+        }),
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    //{ loader: 'resolve-url-loader' },
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                ],
+            },
+            {
+                test: /\.riot$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "@riotjs/webpack-loader",
+                    options: {
+                        hot: false, // set it to true if you are using hmr
+                        // add here all the other @riotjs/compiler options riot.js.org/compiler
+                        // template: 'pug' for example
+                        scopedCss: true
+                    }
+                }]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    resolve: {
+        fallback: {
+            crypto: require.resolve("crypto-browserify"),
+            path:   require.resolve("path-browserify"),
+            stream: require.resolve("stream-browserify"),
+            buffer: require.resolve("buffer/")
+        }
+    }
+};
