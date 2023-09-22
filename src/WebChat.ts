2c53de5ba2adef39010b8af0b146877e9dea512b
Maicon Filippsen <filippsen@universe.ai>
2023-09-22 08:45:27 -0300

    Add UniverseAI imports

---
+++ src/WebChat.ts
@@ -0,0 +1,20 @@
+/**
+ * Globals and helper class.
+ *
+ */
+
+import {
+    BrowserUtil,
+    BrowserFileStreamReader,
+    Service,
+    ParseUtil,
+    Universe,
+} from "universeai";
+
+export default {
+    BrowserUtil,
+    BrowserFileStreamReader,
+    Service,
+    Universe,
+    ParseUtil,
+}
