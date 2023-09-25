a959d4f0163a76b3297d1c28e4f192966e26e40e
Thomas Backlund <bashlund@universe.ai>
2023-09-25 13:23:21 +0200

    Add riot components

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Globals from "../build/Globals-browser.js";
 
 import { minidenticon } from 'minidenticons'
 
+import WebChatChannel from "./webchat-channel.riot";
+
+riot.register("webchat-channel", WebChatChannel);
+
 // Need to call minidenticon to activate it.
 minidenticon();
 
